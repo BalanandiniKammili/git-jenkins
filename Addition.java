c6762bbc2fd10ce86fda8fb24d86278782bd40a4
BalanandiniKammili <112051898+BalanandiniKammili@users.noreply.github.com>
2022-11-23 17:20:11 +0530

    Update Addition.java

--- Addition.java
+++ Addition.java
@@ -4,8 +4,8 @@ public static void main(String[] args)
 {
 int a=5;
 int b=10;
-System.out.println("a:", +a);
-System.out.println("b:", +b);
-System.out.println("a+b:", +(a+b));
+System.out.println("a:" +a);
+System.out.println("b:" +b);
+System.out.println("a+b:" +(a+b));
 }
 }
